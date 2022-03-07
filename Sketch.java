037d9a77e8898865ff59f0020ef1f1709fb39348
Brennan Chan <99412514+BrennanChan2022@users.noreply.github.com>
2022-03-07 21:10:25 +0000

    random locations

--- Sketch.java
+++ Sketch.java
@@ -1,21 +1,21 @@
 import processing.core.PApplet;
 
   /**
-  * A program Sketch.java that draws a house in random locations with random background colours.
+  * A program Sketch.java that draws a house in random locations with random background colours depending on the location.
   * @author: B. Chan
   */
 
 public class Sketch extends PApplet {
 
-  float randHouseX = random(0, 500);
-  float randHouseY = random(0, 500);
+  float randHouseX = random(100, 400);
+  float randHouseY = random(100, 400);
   // rect(130,160,240,240)
   
   public void settings() {
     size(500, 500);
   }
 
-  // Set background to blue
+  // Set background to depending on location
   public void setup() {
     background(176, 227, 255);
   }
@@ -25,10 +25,10 @@ public class Sketch extends PApplet {
     // Draw grass + dirt
     noStroke();
     fill(57, 184, 65);
-    rect(0,400,500,50);
+    rect(randHouseX - 130, randHouseY + 240, 500, 50);
 
     fill(107, 68, 27);
-    rect(0,450,500,50);
+    rect(randHouseX - 130, randHouseY + 290, 500, 50);
  
     // Set stroke settings for house + roof
     stroke(0,0,0);
@@ -36,37 +36,37 @@ public class Sketch extends PApplet {
 
     // Draw house + roof
     fill(148, 61, 61);
-    rect(130,160,240,240);
+    rect(randHouseX, randHouseY, 240, 240);
 
     fill(130, 127, 127);
-    rect(310,80,40,60);
-    triangle(100, 160, 250, 80, 400, 160);
+    rect(randHouseX + 180, randHouseY - 80, 40, 60);
+    triangle(randHouseX - 30, randHouseY, randHouseX + 120, randHouseY - 80, randHouseX + 270, randHouseY);
 
     // Draw door and windows
     fill(79, 5, 5);
-    rect(220,300,60,100);
+    rect(randHouseX + 90, randHouseY + 140, 60, 100);
 
     fill(203, 212, 211);
-    ellipse(180, 270, 50, 60);
-    ellipse(320, 270, 50, 60);
-    rect(220,180,60,50);
+    ellipse(randHouseX + 60, randHouseY + 110, 50, 60);
+    ellipse(randHouseX + 190, randHouseY + 110, 50, 60);
+    rect(randHouseX + 90,randHouseY + 20, 60, 50);
 
     // Draw window panes
     stroke(0,0,0);
-    line(250, 180, 250, 230); 
-    line(220, 205, 280, 205); 
+    line(randHouseX + 120, randHouseY + 20, randHouseX + 120, randHouseY + 70); 
+    line(randHouseX + 90, randHouseY + 45, randHouseX + 150, randHouseY + 45); 
 
-    // Draw sun and clouds
+    // Draw sun
     noStroke();
     fill(255, 251, 33);
-    ellipse(40, 40, 60, 60);
+    ellipse(randHouseX - 90, randHouseY - 120, 60, 60);
 
     // Set variables to current time
     var currentHour = hour();
     var currentMinute = minute();
     var currentSecond = second();
     
-    var currentTime = currentHour + ":" + nf(currentMinute, 2) + ":" + nf(currentSecond,2);
+    var currentTime = nf((currentHour - 5), 2) + ":" + nf(currentMinute, 2) + ":" + nf(currentSecond,2);
     
     fill(255);
     noStroke();
